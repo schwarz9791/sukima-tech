fa2a757f199d270ea21083eb4e026cc5fedae0d8
Masaki Matsumura <mm.schwarz@gmail.com>
2019-02-13 17:29:35 +0900

    ADD: category editing

--- app/pages/admin/categories/_category_id/edit.vue
+++ app/pages/admin/categories/_category_id/edit.vue
@@ -1,14 +1,73 @@
 <template lang="pug">
-  div
-    p edit category - {{ categoryId }}
+  section(
+    v-loading.fullscreen.lock='isLoading'
+    element-loading-text='Loading...'
+  )
+    section
+      p edit category - {{ categoryId }}
+      el-form(v-if='!isLoading' ref='category' :model='category')
+        el-form-item(label='name')
+          el-input(v-model='name')
+        el-form-item(label='order')
+          el-input(v-model='order')
+        el-form-item(label='template')
+          el-input(v-model='template')
+      br
+      nuxt-link(to="/admin") Go admin top
 </template>
 
 <script>
+import { mapState, mapGetters, mapActions } from 'vuex'
+
 export default {
-  data() {
-    return {
-      categoryId: this.$route.params.category_id
+  name: 'Editcategory',
+  computed: {
+    ...mapState('categories', ['category']),
+    isLoading() {
+      return !this.category
+    },
+    categoryId() {
+      return this.$route.params.category_id
+    },
+    name: {
+      get() {
+        return this.category.name
+      },
+      set(value) {
+        this.updateSingle({
+          name: value
+        })
+      }
+    },
+    order: {
+      get() {
+        return this.category.order
+      },
+      set(value) {
+        this.updateSingle({
+          order: value
+        })
+      }
+    },
+    template: {
+      get() {
+        return this.category.template
+      },
+      set(value) {
+        this.updateSingle({
+          template: value
+        })
+      }
     }
+  },
+  created() {
+    this.bindSingle(this.categoryId)
+  },
+  destroyed() {
+    this.unbindSingle()
+  },
+  methods: {
+    ...mapActions('categories', ['bindSingle', 'unbindSingle', 'updateSingle'])
   }
 }
 </script>
--- app/store/categories.js
+++ app/store/categories.js
@@ -5,12 +5,16 @@ const db = firebase.firestore()
 const categoriesRef = db.collection('categories')
 
 export const state = () => ({
-  categories: []
+  categories: [],
+  category: {}
 })
 
 export const getters = {
   categories(state) {
     return state.categories
+  },
+  categoryById: state => id => {
+    return state.categories.find(category => category._id === id)
   }
 }
 
@@ -20,5 +24,15 @@ export const actions = {
   }),
   unbind: firebaseAction(({ unbindFirebaseRef }) => {
     unbindFirebaseRef('categories')
+  }),
+  bindSingle: firebaseAction(({ bindFirebaseRef }, id) => {
+    bindFirebaseRef('category', categoriesRef.doc(id))
+  }),
+  unbindSingle: firebaseAction(({ unbindFirebaseRef }) => {
+    unbindFirebaseRef('category')
+  }),
+  updateSingle: firebaseAction(({ state }, payload) => {
+    const data = _.merge({}, state.category, payload)
+    categoriesRef.doc(state.category.id).set(data)
   })
 }
