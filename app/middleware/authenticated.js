4cd75c3fc809763600a21312ed98c90e73b0cf63
Masaki Matsumura <mm.schwarz@gmail.com>
2019-04-30 16:06:25 +0900

    [skip ci] FIX: remove context

--- app/middleware/authenticated.js
+++ app/middleware/authenticated.js
@@ -1,28 +1,28 @@
 import FirebaseLocalStorageDB from '@/assets/javascripts/FirebaseLocalStorageDB'
 
-export default async function(context) {
+export default async function({ store, route, redirect }) {
   try {
     const db = await FirebaseLocalStorageDB.connect()
     const firebaseData = await db.get(
       `firebase:authUser:${process.env.FIREBASE_API_KEY}:[DEFAULT]`
     )
     if (firebaseData) {
-      context.store.commit('users/user', firebaseData.value)
-      context.store.commit(
+      store.commit('users/user', firebaseData.value)
+      store.commit(
         'users/token',
         firebaseData.value.stsTokenManager.accessToken
       )
     }
-    if (context.store.getters['users/user']) return
+    if (store.getters['users/user']) return
     if (
-      context.route.name !== 'admin' &&
-      context.route.name !== 'admin-logout' &&
-      context.route.name.includes('admin')
+      route.name !== 'admin' &&
+      route.name !== 'admin-logout' &&
+      route.name.includes('admin')
     ) {
       throw new Error('Please login.')
     }
   } catch (e) {
     alert(e.message)
-    context.redirect(`/admin?rd=${context.route.path}`)
+    redirect(`/admin?rd=${route.path}`)
   }
 }
