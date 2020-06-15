91a79280f34fa27f9a2c06bdacbcf7d9c291726e
Masaki Matsumura <mm.schwarz@gmail.com>
2020-06-15 16:32:39 +0900

    FIX: Remove Firebase environment variables from the repository

--- config/env.development.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  BASE_URL: '',
-  FIREBASE_API_KEY: 'AIzaSyCTB4LQ4gWeHBXADkzA89QZy8yUc7zCmFk',
-  FIREBASE_AUTH_DOMAIN: 'stg-sukima-tech.firebaseapp.com',
-  FIREBASE_DATABASE_URL: 'https://stg-sukima-tech.firebaseio.com',
-  FIREBASE_PROJECT_ID: 'stg-sukima-tech',
-  FIREBASE_STORAGE_BUCKET: 'stg-sukima-tech.appspot.com',
-  FIREBASE_MESSAGING_SENDER_ID: '206811086107'
-}
--- config/env.production.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  BASE_URL: 'https://sukima.tech',
-  FIREBASE_API_KEY: 'AIzaSyCYpLdnVN5jA1UsRxe0Z7fuXDZVmZOmtOk',
-  FIREBASE_AUTH_DOMAIN: 'sukima-tech.firebaseapp.com',
-  FIREBASE_DATABASE_URL: 'https://sukima-tech.firebaseio.com',
-  FIREBASE_PROJECT_ID: 'sukima-tech',
-  FIREBASE_STORAGE_BUCKET: 'sukima-tech.appspot.com',
-  FIREBASE_MESSAGING_SENDER_ID: '229122984987'
-}
